1b13184f0efb0b0fa2b8bfcc40611e3f605c265c
glombardo-ciclogreen <gianf.lombardo@gmail.com>
2024-06-05 19:29:52 +0200

    Dynamic config form Render

--- manage.py
+++ manage.py
@@ -4,9 +4,16 @@
 
 import os
 import sys
+import environ
+
+env = environ.Env()
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+    DEBUG = env.bool('DJANGO_DEBUG', False)
+    
+    config = "config.settings.local" if DEBUG else "config.settings.production"
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", config)
+
 
     try:
         from django.core.management import execute_from_command_line
