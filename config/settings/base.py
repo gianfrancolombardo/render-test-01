f080624db508debcd90d65b3a02a617cfa6397af
glombardo-ciclogreen <gianf.lombardo@gmail.com>
2024-06-05 16:49:53 +0200

    First commit :smiley:

---
+++ config/settings/base.py
@@ -0,0 +1,167 @@
+"""Base settings to build other settings files upon."""
+
+import environ
+
+ROOT_DIR = environ.Path(__file__) - 3
+APPS_DIR = ROOT_DIR.path('render_test_01')
+
+env = environ.Env()
+
+# Base
+DEBUG = env.bool('DJANGO_DEBUG', False)
+
+# Language and timezone
+TIME_ZONE = 'Europe/Madrid'
+LANGUAGE_CODE = 'es'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# DATABASES
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(
+        default=env.db('DATABASE_URL'),
+        conn_max_age=600
+    ),
+}
+DATABASES['default']['ATOMIC_REQUESTS'] = True
+
+# URLs
+ROOT_URLCONF = 'config.urls'
+
+# WSGI
+WSGI_APPLICATION = 'config.wsgi.application'
+
+# Users & Authentication
+AUTH_USER_MODEL = 'users.User'
+
+# Apps
+DJANGO_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    'corsheaders',
+]
+LOCAL_APPS = [
+    'render_test_01.users.apps.UsersAppConfig',
+    'render_test_01.api.apps.ApiAppConfig'
+]
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+# Passwords
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Middlewares
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+]
+
+# Static files
+STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    str(APPS_DIR.path('static')),
+]
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Media
+MEDIA_ROOT = str(APPS_DIR('media'))
+MEDIA_URL = '/media/'
+
+# Templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            str(APPS_DIR.path('templates')),
+        ],
+        'OPTIONS': {
+            'debug': DEBUG,
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ],
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Security
+SESSION_COOKIE_HTTPONLY = True
+CSRF_COOKIE_HTTPONLY = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+
+# Email
+EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+
+# Admin
+ADMIN_URL = 'admin/'
+ADMINS = [
+    ("""Admin""", 'admin@example.com'),
+]
+MANAGERS = ADMINS
+
+# Django CROS
+CORS_ORIGIN_WHITELIST = env.list('DJANGO_CORS_ORIGIN', default=[])
+
+# Django AutoFiled
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
---
+++ render_test_01/api/apps.py
@@ -0,0 +1,10 @@
+"""Api app."""
+
+# Django
+from django.apps import AppConfig
+
+class ApiAppConfig(AppConfig):
+    """api app config."""
+
+    name = 'render_test_01.api'
+    verbose_name = 'Api'
